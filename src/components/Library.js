ec90a37d21a43b940a022f8fb998d7d4d5ecc676
vdowsett <vdowsett@gmail.com>
2020-12-08 15:39:12 -0600

    added routing, landing, library components

---
+++ src/components/Library.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Plant from './Plant.js';
+
+const Library = () => (
+  <section className="library">
+    Library will go here
+    <Plant></Plant>
+  </section>
+);
+
+export default Library;
