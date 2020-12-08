a95e9e31d4e49804bd15dfed85a863fc69126c5f
vdowsett <vdowsett@gmail.com>
2020-12-08 15:09:25 -0600

    plant component created

---
+++ src/components/plant.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Plant extends Component {
+    render() {
+        return (
+          <li> A plant will go here </li>
+        );
+      }
+}
+
+export default Plant;
